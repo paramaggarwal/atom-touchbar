c5e7078b6006c6d77043d4461993c565596cb6e0
Tomas Gajdos <Tomas_Gajdos@mckinsey.com>
2017-07-18 01:59:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# atom-touchbar package
+
+An Atom package adding a couple of useful buttons to MacBook TouchBar.
+Non-customizable. Just a basic quick implementation. 
+
+## Requirements
+* Atom 1.19beta
---
+++ spec/atom-touchbar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTouchbarView from '../lib/atom-touchbar-view';
+
+describe('AtomTouchbarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
